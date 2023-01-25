ac093084bc1ab3b10f3598d975514b76723d05a5
Javier <jmartin@grupoloyal.com>
2023-01-25 18:38:42 +0100

    labs 08-09

--- sql/ejemplos_04.sql
+++ sql/ejemplos_04.sql
@@ -1,14 +1,41 @@
+SELECT d.*
+FROM hr.employees e 
+    RIGHT JOIN hr.departments d ON e.department_id = d.department_id
+WHERE e.employee_id is null;
+
+SELECT *
+FROM hr.departments
+WHERE department_id not in (
+    SELECT distinct department_id
+    FROM hr.employees
+    WHERE department_id is not null
+    --order by department_id
+);
+
+SELECT *
+FROM hr.departments d
+WHERE NOT EXISTS(
+    SELECT 1
+    FROM hr.employees e
+    WHERE d.department_id = e.department_id and e.department_id is not null
+);
+
+
 SELECT *
 from employees e 
     INNER JOIN jobs j ON j.job_id = e.job_id
 WHERE e.salary NOT BETWEEN j.min_salary and j.max_salary;
 
 SELECT DISTINCT m.*
-FROM employees w JOIN employees m ON w.manager_id = m.employee_id;
+FROM hr.employees w JOIN hr.employees m ON w.manager_id = m.employee_id;
 
-SELECT DISTINCT manager_id 
-FROM employees 
-WHERE manager_id IS NOT NULL;
+SELECT *
+FROM hr.employees
+where employee_id in (
+    SELECT manager_id 
+    FROM hr.employees 
+    WHERE manager_id IS NOT NULL
+);
 
 SELECT salary
 FROM employees m
@@ -37,13 +64,46 @@ ORDER BY 2;
 
 SELECT e.last_name "Employee", salary, (SELECT avg(salary) FROM employees i WHERE i.job_id = e.job_id) media
 FROM employees e
+where (SELECT avg(salary) FROM employees i WHERE i.job_id = e.job_id) > 4000
 ORDER BY 1;
 
 SELECT e.last_name, salary, media
 FROM employees e JOIN (
-    SELECT job_id, avg(salary) media
-    FROM employees
-    GROUP BY job_id
+        SELECT job_id, avg(salary) media
+        FROM employees
+        GROUP BY job_id
     ) sal ON e.job_id = sal.job_id
-ORDER BY 1
+where media > 4000
+ORDER BY 1;
 
+SELECT e.last_name, salary, media
+FROM employees e JOIN (
+        SELECT job_id, avg(salary) media
+        FROM employees
+        GROUP BY job_id
+    ) sal ON e.job_id = sal.job_id
+ORDER BY 1;
+
+SELECT d.department_id id_dept, d.department_name, 1 consulta
+FROM departments d 
+    JOIN locations l USING (location_id)
+WHERE LOWER(l.city) = 'toronto'
+MINUS
+SELECT 60, 'IT', 2 from dual
+UNION
+SELECT d.department_id, d.department_name, 2
+FROM employees e 
+    JOIN departments d ON e.department_id = d.department_id
+WHERE e.job_id = 'IT_PROG'
+ORDER BY 1
+UNION
+SELECT 1 department_id, 'Falso' from dual
+ORDER BY 1;
+select department_id, last_name, salary
+from (SELECT department_id, last_name, salary, 1 orden
+FROM employees e
+UNION
+SELECT department_id, 'TOTAL', sum(salary), 2
+FROM employees e
+GROUP BY department_id
+Order by department_id,orden, last_name);
