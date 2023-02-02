7488069ca964d79c7126f08c70eac239596ba6fc
Javier <jmartin@grupoloyal.com>
2023-02-02 19:44:35 +0100

    multiples

---
+++ plsql/demos4.pl.sql
@@ -0,0 +1,72 @@
+CLEAR SCREEN
+
+DECLARE
+    TYPE tabla_type IS TABLE OF VARCHAR2(100) INDEX BY PLS_INTEGER;
+--    TYPE tabla_type IS TABLE OF VARCHAR2(100);
+--    TYPE tabla_type IS VARRAY(15) OF VARCHAR2(100);
+    tabla tabla_type := tabla_type('uno', 'dos', 'tres', 'cuatro');
+
+    PROCEDURE print_secuencial(heading VARCHAR2) IS
+    BEGIN
+        dbms_output.put_line(heading);
+--        FOR indice IN 1..tabla.count LOOP
+--            dbms_output.put_line(indice || ' => ' || tabla(indice));
+--        END LOOP;
+--        FOR indice IN tabla.first .. tabla.last LOOP
+--            if tabla.exists(indice) then
+--                dbms_output.put_line(indice || ' => ' || tabla(indice));
+--            end if;
+--        END LOOP;
+--       FOR indice IN INDICES OF tabla LOOP 
+--           DBMS_OUTPUT.PUT_LINE(indice || ' => '|| tabla(indice));  
+--       END LOOP;
+--       FOR valor IN VALUES OF tabla LOOP
+--          DBMS_OUTPUT.PUT_LINE(valor);
+--       END LOOP;
+       FOR indice, valor IN PAIRS OF tabla LOOP
+          DBMS_OUTPUT.PUT_LINE(indice || ' => '|| valor);
+          exit when valor > 'Algo';
+       END LOOP;
+       
+        dbms_output.put_line('---');
+    END;
+
+    PROCEDURE print_por_index(heading VARCHAR2) IS
+    indice  NUMBER;
+    BEGIN
+        dbms_output.put_line(heading);
+        indice := tabla.first;
+        WHILE indice IS NOT NULL LOOP
+            dbms_output.put_line(indice || ' => ' || tabla(indice));
+            indice := tabla.next(indice);
+        END LOOP;
+
+        dbms_output.put_line('---');
+    END;
+
+BEGIN
+--    tabla(1) := 'UNO';
+--    tabla(2) := 'dos';
+--    tabla(3) := 'tres';
+--    tabla(4) := 'cuatro';
+--    tabla := tabla_type('uno', '22', 3 => 'tres', 5 => 'cinco'); --, OTHERS => 'Otros');
+--    tabla := tabla_type('uno', 3 => 'tres', 5 => 'cinco', OTHERS => 'Otros');
+--    tabla := tabla_type(FOR i IN 1..5 SEQUENCE => 'ALUM ' || i);
+--    tabla := tabla_type(FOR i IN 2..10 BY 2 INDEX i/2 => 'ALUM ' || i);
+--    tabla := tabla_type(FOR i IN 2..10 BY 2 => 'ALUM ' || i);
+    print_secuencial('Orginal');
+--    tabla := tabla_type('uno', 'dos', 'tres', 'cuatro', 'cinco');
+/*
+tabla(1) := '1111';
+    tabla.extend;
+    tabla(5) := 'cinco';
+    tabla.extend;
+    tabla(6) := 'seis';
+    tabla.delete(2, 4);
+--    tabla.trim(2);
+--    tabla.extend;
+    --print_secuencial('Actual');
+    print_por_index('Actual');
+*/
+END;
+
---
+++ plsql/demos5.pl.sql
@@ -0,0 +1,56 @@
+CLEAR SCREEN
+/*
+DECLARE
+   CURSOR c_emp_cursor IS 
+       SELECT employee_id, last_name 
+       FROM employees
+       WHERE  department_id =30;
+    TYPE tabla_type IS TABLE OF c_emp_cursor%ROWTYPE INDEX BY PLS_INTEGER;
+    empleados tabla_type;
+    v_empno employees.employee_id%TYPE;
+    v_lname employees.last_name%TYPE;
+    v_emp_record	c_emp_cursor%ROWTYPE;
+BEGIN
+  OPEN c_emp_cursor;
+  LOOP
+--    FETCH c_emp_cursor 
+--    INTO v_empno, v_lname;
+--    EXIT WHEN c_emp_cursor%NOTFOUND;
+--    DBMS_OUTPUT.PUT_LINE( v_empno ||' '||v_lname);    
+    FETCH c_emp_cursor 
+    INTO v_emp_record;
+    EXIT WHEN c_emp_cursor%NOTFOUND;
+    empleados(v_emp_record.employee_id) := v_emp_record;
+--    DBMS_OUTPUT.PUT_LINE( v_emp_record.employee_id ||' '||v_emp_record.last_name);    
+  END LOOP;
+  CLOSE c_emp_cursor;
+  
+  for emp in VALUES OF empleados LOOP
+    DBMS_OUTPUT.PUT_LINE( emp.employee_id ||' '||emp.last_name);
+  end loop;
+  DBMS_OUTPUT.PUT_LINE( '......');
+  for emp in VALUES OF empleados LOOP
+    DBMS_OUTPUT.PUT_LINE( emp.employee_id ||' '||emp.last_name);
+  end loop;
+  DBMS_OUTPUT.PUT_LINE( '......');
+  DBMS_OUTPUT.PUT_LINE( empleados(118).employee_id ||' '||empleados(118).last_name);
+END;
+DECLARE
+   CURSOR c_emp_cursor IS 
+       SELECT employee_id, last_name 
+       FROM employees
+       WHERE  department_id =30;
+BEGIN
+  for v_emp_record in c_emp_cursor loop
+    DBMS_OUTPUT.PUT_LINE( v_emp_record.employee_id ||' '||v_emp_record.last_name);
+  end loop;
+END;
+*/
+
+BEGIN
+  for v_emp_record in (SELECT employee_id, last_name 
+       FROM employees
+       WHERE  department_id =30) loop
+    DBMS_OUTPUT.PUT_LINE( v_emp_record.employee_id ||' '||v_emp_record.last_name);
+  end loop;
+END;
