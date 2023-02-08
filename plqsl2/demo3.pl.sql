2e9a56b69c1e3859cbee174acb556af54d170cc4
Javier <jmartin@grupoloyal.com>
2023-02-08 15:57:45 +0100

    trigger

--- plqsl2/demo3.pl.sql
+++ plqsl2/demo3.pl.sql
@@ -1,9 +1,16 @@
---select department_id, department_name, CUENTA_EMP(department_id) empleados
---from departments;
-SELECT CUENTA_EMP from dual;
+/*
+select department_id, department_name, CUENTA_EMP(department_id) empleados
+from departments
+where VALID_DEPTID(department_id);
+SELECT CUENTA_EMP, VALID_DEPTID(999) from dual;
 
+select department_id, department_name, CUENTA_EMP(department_id) empleados
+from departments
+where VALID_DEPTID_SQL(department_id) = 1;
+SELECT CUENTA_EMP, VALID_DEPTID_SQL(999) from dual;
+*/
 clear SCREEN;
-
+/*
 declare
     v_emps number;
     FUNCTION nombre 
@@ -28,7 +35,7 @@ end;
 --    END)
 --select nombre(first_name, last_name) 
 --from employees;
-
+*/
 /*
 CREATE OR REPLACE FUNCTION CUENTA_EMP 
 (
@@ -43,4 +50,23 @@ BEGIN
   --insert INTO messages(results) values('Cuenta: ' || cont);
   RETURN cont;
 END CUENTA_EMP;
+*/
+
+BEGIN
+    insert INTO messages(results) values('SI O SI antes de add_job');
+    --add_job('AD_VP', 'kk');
+    add_job('KK_KKK', 'kk', TRUE);
+    insert INTO messages(results) values('SI O SI despues de add_job');
+    ROLLBACK;
+EXCEPTION
+    when OTHERS then
+        DBMS_OUTPUT.PUT_LINE('Externo: ' || SQLCODE || ', ' || SQLERRM);
+        ROLLBACK;
+END;
+
+/*
+select * from messages order by cuando desc;
+select * from jobs;
+delete from jobs where upper(job_id) like 'KK%';
+
 */
